224c903dd251e0f3bfbc174c993f43f71aa215a0
Fernando Perez <Fernando.Perez@berkeley.edu>
2011-10-21 10:09:44 -0700

    Update win32 batch file.

--- book/notebooks/ipython-notebook.bat
+++ book/notebooks/ipython-notebook.bat
@@ -1,5 +1,18 @@
 @rem IPython Notebook startup file (with pylab activated by default)
-@echo ***********************************************************************
-@echo In order to stop the IPython notebook, press Control-C.
-@echo ***********************************************************************
-@ipython notebook --pylab inline --no-browser --notebook-dir %~dp0
+@echo off
+@echo.
+@echo ********************************************************************
+@echo.
+@echo            Welcome to the IPython Notebook Environment
+@echo.
+@echo   Please open Chrome or Firefox and open the following address:
+@echo.  
+@echo                     http://127.0.0.1:8888
+@echo.
+@echo   When finished, stop the notebook by pressing in this window:
+@echo.
+@echo                          Control-C
+@echo.
+@echo ********************************************************************
+@echo.
+@ipython notebook --pylab inline --no-browser --port 8888 --notebook-dir %~dp0
