b43b74b0d30596e3ba676b86f5b10f45b258a8e6
Fernando Perez <Fernando.Perez@berkeley.edu>
2011-10-22 02:04:41 -0700

    Add notebooks for tomorrow.

---
+++ notebooks/template.py
@@ -0,0 +1,37 @@
+# <nbformat>2</nbformat>
+
+# <markdowncell>
+
+# # Template tools for notebooks
+# 
+# **Links:** [IPython](http://ipython.org),
+# [NumPy](http://numpy.org), [SciPy](http://scipy.org), [matplotlib](http://matplotlib.sf.net)
+# 
+# <br/>
+# [Python(x,y)](http://www.pythonxy.com) and [EPD](http://www.enthought.com/products/epd.php) provide
+# one-click installers.
+# 
+# <style type="text/css">
+#     .exercise {
+#         border: solid 3px #AAE;
+#         background: #EEF;
+#         padding: 50px 10px 50px 10px;
+#         display: inline-block;
+#         font-size: 110%;
+#     }
+# </style>
+
+# <codecell>
+
+# a normal code cell
+print 'hi'
+
+# <markdowncell>
+
+# <span class="exercise">
+# **Exercise:** An exercise cell
+# </span>
+
+# <codecell>
+
+
