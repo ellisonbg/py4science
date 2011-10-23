481d8ce142e0ee2bacc6aa0e390bf09cea37855c
Fernando Perez <Fernando.Perez@berkeley.edu>
2011-10-23 15:55:19 -0700

    Small fixes to sound compression script and notebook.

--- book/examples/fourier_sound_compress.py
+++ book/examples/fourier_sound_compress.py
@@ -30,20 +30,20 @@ from scipy.io import wavfile
 #-----------------------------------------------------------------------------
 # Function definitions
 #-----------------------------------------------------------------------------
-
-def fourier_approx(x, frac=0.5, makeplot=True, Fs=11000):
+def fourier_approx(x, frac=0.5, Fs=11000):
     """
     approximate x by taking only the frac largest fourier components where frac
     is a float between 0..1
 
     Fs is the sampling frequency of the data
-
+    
     return the approximated signal
 
     if makeplot is True, plot the original time series and approximation in a
     pyplot figure
     """
-    out = np.fft.rfft(x)
+    out = np.fft.rfft(x) 
+    print 'rfft', x.shape, out.shape
     # normally the output of fft is the same size as the input, but
     # for real inputs, the fft is symmetric around the DC component,
     # so rfft is a helper function which only returns the positive
@@ -56,39 +56,37 @@ def fourier_approx(x, frac=0.5, makeplot=True, Fs=11000):
     indmin = int((1-frac)*len(magnitude))
     out[ind[:indmin]] = 0.
 
-    xapprox = np.fft.irfft(out)
+    ret = np.fft.irfft(out)
+    print 'irrft', ret.shape, out.shape
+    return ret
 
-    if makeplot:
-        # x and xapprox could be of different lengths, so we trim all
-        # variables for plotting to be of the same length
-        common_len = min(len(x), len(xapprox))
-        x = x[:common_len]
-        xapprox = xapprox[:common_len]
 
-        # Now, build the figure for plotting
-        t = np.arange(len(x), dtype=float)/Fs
-        fig = plt.figure()
-        plt.plot(t, x, label='original', lw=1)
-        plt.plot(t, xapprox, label='approx', lw=1)
-        plt.title('Approx signal with %d frequencies (%.2f%% of the total)'%(
-            indmin, 100*frac))
-        plt.xlabel('time (s)')
+def plot_approx(x, xapprox, frac=0.5, Fs=11000):
+    """Plot an audio signal and its approximation.
+    """
+    t = np.arange(len(x), dtype=float)/Fs
 
-        plt.grid()
-        leg = plt.legend(fancybox=True)
-        leg.get_frame().set_alpha(0.5)
+    fig = plt.figure()
+    #print 'x', t.shape, x.shape, xapprox.shape
+    plt.plot(t, x, label='original', lw=1)
+    
+    #print 'xapprox', t.shape, xapprox.shape
+    plt.plot(t, xapprox, label='approx', lw=1)
+    plt.title('Approx signal %.2f%% of the total frequencies'%(
+        100*frac))
+    plt.xlabel('time (s)')
 
-        fig = plt.figure()
-        ax1 = plt.subplot(211)
-        plt.psd(x, Fs=Fs)
-        plt.ylabel('original')
-        plt.title('Power spectrum of original and approx')
-        plt.xlabel('')
-        ax2 = plt.subplot(212,sharex=ax1)
-        plt.psd(xapprox, Fs=Fs)
-        plt.ylabel('approx')
+    plt.grid()
+    leg = plt.legend(fancybox=True)
+    leg.get_frame().set_alpha(0.5)
 
-    return xapprox
+    fig, (ax0, ax1) = plt.subplots(2, 1)
+    ax0.psd(x, Fs=Fs)
+    ax0.set_ylabel('original')
+    ax0.set_title('Power spectrum of original and approx')
+    ax0.set_xlabel('')
+    ax1.psd(xapprox, Fs=Fs)
+    ax1.set_ylabel('approx')
 
 #-----------------------------------------------------------------------------
 # Main script
@@ -103,20 +101,27 @@ if __name__ == '__main__':
     # Fraction of frequencies to keep (as a number in [0,1]).
     frac = 0.2
 
+    # Load data file
     basename, ext = os.path.splitext(infile)
-
     rate, x = wavfile.read(infile)
+
+    # Note: the file may have an even or odd number of samples, but if the
+    # length is odd, some care must be taken later on when applying the inverse
+    # ral-only FFT.  For simplicity, we simply drop the last data point if the
+    # file is even.
+    if len(x) %2:  x = x[:-1]
 
     if len(x.shape)==2:
         # looks like a stereo wave
         print 'extracting mono channel'
         x = x[:,0]
 
-    xapprox = fourier_approx(x, frac=frac, makeplot=True)
-
+    xapprox = fourier_approx(x, frac=frac)
+    plot_approx(x, xapprox, frac, rate)
+    
     # linearly rescale raw data to wav range and convert to integers
     sound = xapprox.astype(np.int16)
-
+    
     wavfile.write('%s_frac%d.wav' % (basename, 100*frac), rate, sound)
 
     plt.show()
